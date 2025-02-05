71b5618e0f80b6f4c0da2402aa49e7e057ca827e
MarcosX <markito5_net@hotmail.com>
2025-02-04 22:25:20 -0300

    size modal news

--- app/components/GetNews.jsx
+++ app/components/GetNews.jsx
@@ -9,7 +9,7 @@ export const GetNews = () => {
     }
 
     return (
-        <div id="newsModal" className="fixed top-[9%] left-[36%] w-[36%] h-[80%] border-l-8 border-[#394E99] rounded-lg bg-white shadow-lg p-3 overflow-y-auto hidden ">
+        <div id="newsModal" className="fixed top-[9%] left-[30%] w-[45%] h-[80%] border-l-8 border-[#394E99] rounded-lg bg-white shadow-lg p-3 overflow-y-auto hidden ">
             <div className="w-full flex justify-between items-center mb-3 sticky">
                 <h2 className="w-auto size-4 font-bold">Últimas Noticas</h2>
                 <button id="closeModal" onClick={closeModal} className="text-dark-900 text-xl font-bold">&times;</button>
